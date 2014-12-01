c1fb7ea69676dbdd13c3b8e12f5ae81e711854d8
Pablo Mantellini <pmantellini@mulesoft.com>
2014-12-01 15:54:12 -0300

    updated license notice

--- using-transactional-scope-in-jms-to-database/src/main/java/org/mule/examples/TestComponent.java
+++ using-transactional-scope-in-jms-to-database/src/main/java/org/mule/examples/TestComponent.java
@@ -1,6 +1,6 @@
 /**
  * MuleSoft Examples
- * Copyright [2014] MuleSoft, Inc.
+ * Copyright 2014 MuleSoft, Inc.
  *
  * This product includes software developed at
  * MuleSoft, Inc. (http://www.mulesoft.com/).
