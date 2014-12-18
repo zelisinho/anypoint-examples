c0133fb4ba9f41f58b0e607f1ee072c757a5c178
Lenka Mizikova <lenka.mizikova@hotovo.org>
2014-12-18 09:58:20 +0100

    Changed host from localhost to 0.0.0.0

--- foreach-processing-and-choice-routing/src/test/java/org/mule/examples/ForEachProcessingAndChoiceRoutingTest.java
+++ foreach-processing-and-choice-routing/src/test/java/org/mule/examples/ForEachProcessingAndChoiceRoutingTest.java
@@ -36,7 +36,7 @@ public class ForEachProcessingAndChoiceRoutingTest extends FunctionalTestCase
         MuleClient client = new MuleClient(muleContext);
         Map<String, Object> props = new HashMap<String, Object>();
         props.put("http.method", "GET");
-        MuleMessage result = client.send("http://localhost:11081/?" + MESSAGE, "", props);
+        MuleMessage result = client.send("http://0.0.0.0:11081/?" + MESSAGE, "", props);
         assertNotNull(result);
         assertFalse(result.getPayload() instanceof NullPayload);
         assertTrue(result.getPayloadAsString().contains("Bank"));
