5ba517c5a1afc46be7d7274d4950a85ffd19a4e7
Lenka Mizikova <lenka.mizikova@hotovo.org>
2014-12-18 09:55:35 +0100

    Changed host from localhost to 0.0.0.0

--- querying-a-mysql-database/src/test/java/org/mule/examples/QueryingMysqlDatabaseIT.java
+++ querying-a-mysql-database/src/test/java/org/mule/examples/QueryingMysqlDatabaseIT.java
@@ -61,7 +61,7 @@ public class QueryingMysqlDatabaseIT extends FunctionalTestCase
         MuleClient client = new MuleClient(muleContext);
         Map<String, Object> props = new HashMap<String, Object>();
         props.put("http.method", "GET");
-        MuleMessage result = client.send("http://localhost:8081/?" + MESSAGE, "", props);
+        MuleMessage result = client.send("http://0.0.0.0:8081/?" + MESSAGE, "", props);
         assertNotNull(result);
         assertFalse(result.getPayload() instanceof NullPayload);
         assertEquals(REPLY, result.getPayloadAsString());
