2edd5f5ea5f1e06e24473e3a45dbbac345989c1d
Miroslav Rusin <miroslav.rusin@hotovo.org>
2015-01-28 10:44:44 +0100

    SIT-871, licensing and mvn dependency added

--- salesforce-id-retrieval/src/test/java/org/mule/examples/SalesforceIdRetrievalIT.java
+++ salesforce-id-retrieval/src/test/java/org/mule/examples/SalesforceIdRetrievalIT.java
@@ -1,3 +1,11 @@
+/**
+ * MuleSoft Examples
+ * Copyright 2014 MuleSoft, Inc.
+ *
+ * This product includes software developed at
+ * MuleSoft, Inc. (http://www.mulesoft.com/).
+ */
+
 package org.mule.examples;
 
 import static org.junit.Assert.assertEquals;
