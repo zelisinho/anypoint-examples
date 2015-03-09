65ad2c5f9ea6c124bf1ee12208c3349511543105
Miroslav Rusin <miroslav.rusin@hotovo.org>
2015-03-09 10:40:05 +0100

    SIT-976, fixing a test

--- get-customer-list-from-netsuite/src/test/java/org/mule/examples/GetCustomersListFromNetsuiteIT.java
+++ get-customer-list-from-netsuite/src/test/java/org/mule/examples/GetCustomersListFromNetsuiteIT.java
@@ -57,7 +57,7 @@ public class GetCustomersListFromNetsuiteIT extends FunctionalTestCase
     	MuleClient client = new MuleClient(muleContext);
     	Map<String, Object> props = new HashMap<String, Object>();
         props.put("http.method", "POST");
-        MuleMessage response = client.send("http://0.0.0.0:8081/customers?lastName=" + LAST_NAME_VALUE, "", props);        
+        MuleMessage response = client.send("http://localhost:8081/customers?lastName=" + LAST_NAME_VALUE, "", props);        
         assertTrue(response.getPayloadAsString().contains(TEST_HTML));        										
         
     }
