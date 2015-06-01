2452ea048e2fef2b3359df7c45f7d782acd9b95a
Miroslav Rusin <miroslav.rusin@hotovo.org>
2015-06-01 14:11:45 +0200

    SIT-1044, new example added

---
+++ import-leads-into-salesforce/src/test/java/org/mule/examples/ImportLeadsToSfdcIT.java
@@ -0,0 +1,115 @@
+/**
+ * MuleSoft Examples
+ * Copyright 2014 MuleSoft, Inc.
+ *
+ * This product includes software developed at
+ * MuleSoft, Inc. (http://www.mulesoft.com/).
+ */
+
+package org.mule.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.config.MuleProperties;
+import org.mule.module.client.MuleClient;
+import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.util.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ImportLeadsToSfdcIT extends FunctionalTestCase
+{
+
+	private static final String PATH_TO_TEST_PROPERTIES = "./src/test/resources/mule.test.properties";
+	private static final Logger log = LogManager.getLogger(ImportLeadsToSfdcIT.class); 
+	
+	private static final String EMAIL1 = "faucibus@egetmetus.org";
+	private static final String EMAIL2 = "sem.egestas@mollis.org";
+	
+	private static List<String> leadIds = new ArrayList<String>();
+	private static final String MAPPINGS_FOLDER_PATH = "./mappings";
+
+	 
+    @Override
+    protected String getConfigResources()
+    {
+        return "import-leads-into-salesforce.xml,testflows/test-flows.xml";
+    }
+
+    @BeforeClass
+	public static void init() {
+    	final Properties props = new Properties();
+    	try {
+    	props.load(new FileInputStream(PATH_TO_TEST_PROPERTIES));
+    	} catch (Exception e) {
+    		log.error("Error occured while reading mule.test.properties", e);
+    	}    	
+    	System.setProperty("sfdc.user", props.getProperty("sfdc.user"));
+		System.setProperty("sfdc.password", props.getProperty("sfdc.password"));
+		System.setProperty("sfdc.securityToken", props.getProperty("sfdc.securityToken"));	
+		
+	}
+        
+	@Override
+	protected Properties getStartUpProperties() {
+		Properties properties = new Properties(super.getStartUpProperties());
+
+		String pathToResource = MAPPINGS_FOLDER_PATH;
+		File graphFile = new File(pathToResource);
+
+		properties.put(MuleProperties.APP_HOME_DIRECTORY_PROPERTY,
+				graphFile.getAbsolutePath());
+
+		return properties;
+	}
+    
+    @Test
+    public void testImport() throws Exception
+    {
+    	
+        MuleClient client = new MuleClient(muleContext);
+        String fileInputPath = "file://./src/main/resources/input";
+        String payload = IOUtils.getResourceAsString(
+                "leads.csv", this.getClass());
+        client.dispatch(fileInputPath, payload, null);
+
+        Thread.sleep(8000);
+        SubflowInterceptingChainLifecycleWrapper select = getSubFlow("selectLeadFromSalesforce");        										
+        select.initialise();
+        
+        MuleEvent response = select.process(getTestEvent(EMAIL1, MessageExchangePattern.REQUEST_RESPONSE));        
+        Map<String, Object> lead = (Map<String, Object>)response.getMessage().getPayload();        
+        assertEquals("Ishmael", lead.get("FirstName"));
+        leadIds.add(lead.get("Id").toString());
+        response = select.process(getTestEvent(EMAIL2, MessageExchangePattern.REQUEST_RESPONSE));        
+        lead = (Map<String, Object>)response.getMessage().getPayload();        
+        assertEquals("Burks", lead.get("LastName"));
+        leadIds.add(lead.get("Id").toString());
+                										
+    }
+
+    @After
+    public void tearDown() {
+    	SubflowInterceptingChainLifecycleWrapper delete = getSubFlow("deleteLeadFromSalesforce");
+        try {
+			delete.initialise();
+			delete.process(getTestEvent(leadIds, MessageExchangePattern.REQUEST_RESPONSE));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        		          
+    }
+}
