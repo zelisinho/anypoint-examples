752cda0a118be1463f1f8dad7fc63cb57abf2dbf
Pablo Mantellini <pmantellini@mulesoft.com>
2015-06-29 17:16:38 -0300

    Cleaned up pom and added dw. Replaced expression-component with dw transformer. Updated tests.

--- get-customer-list-from-netsuite/src/test/java/org/mule/examples/GetCustomersListFromNetsuiteIT.java
+++ get-customer-list-from-netsuite/src/test/java/org/mule/examples/GetCustomersListFromNetsuiteIT.java
@@ -60,9 +60,7 @@ public class GetCustomersListFromNetsuiteIT extends FunctionalTestCase
     	MuleClient client = new MuleClient(muleContext);
     	Map<String, Object> props = new HashMap<String, Object>();
         props.put("http.method", "GET");
-        MuleMessage response = client.send("http://localhost:" + HTTP_PORT + "/customers?lastName=" + LAST_NAME_VALUE, "", props);        
-        assertTrue(response.getPayloadAsString().contains(TEST_HTML));        										
-        
+        MuleMessage response = client.send("http://localhost:" + HTTP_PORT + "/customers?lastName=" + LAST_NAME_VALUE, "", props, 60000);
+        assertTrue(response.getPayloadAsString().contains(TEST_HTML));
     }
-
 }
